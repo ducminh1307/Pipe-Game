b1d1981364ad99a62b463851bd35d5ddf896e3e6
sovainoiii <bechuotbi@gmail.com>
2024-06-07 23:41:16 +0700

    add comment code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Pipe _cellPrefab;
 
     private bool hasGameFinished;
-    private Pipe[,] pipes;
+    private Pipe[,] pipes; // Khai bao mang 2 chieu
     private List<Pipe> startPipes;
 
 
@@ -24,31 +24,39 @@ public class GameManager : MonoBehaviour
         SpawnLevel();
     }
 
+    // Ham sinh cap do choi
     private void SpawnLevel()
     {
-        pipes = new Pipe[_level.row, _level.column];
+        pipes = new Pipe[_level.row, _level.column]; // Khoi tao mang 2 chieu co chieu dai la _level.column phan tu, chieu rong la _level.row phan tu
         startPipes = new List<Pipe>();
 
         for (int i = 0; i < _level.row; i++)
         {
             for (int j = 0; j <  _level.column; j++)
             {
+                //Tinh toan toa do sinh ra cua Cell
                 Vector2 spawnPos = new Vector2(j + .5f, i + .5f);
+
+                //Tao Cell voi toa do da tinh
                 Pipe tempPipe = Instantiate(_cellPrefab);
                 tempPipe.transform.position = spawnPos;
+
+                //Khoi tao pipe bang gia tri dua vao cac phan tu cua _level.datas
                 tempPipe.Init(_level.data[i * _level.column + j]);
 
+                //Dua du lieu cua Cell da tao vao trong mang 2 chieu pipes
                 pipes[i, j] = tempPipe;
 
+                //Kiem tra neu la pipe_1 ti dua no vao list startPipes va dat no la ong nuoc bat dau
                 if (tempPipe.pipeType == 1)
-                {
                     startPipes.Add(tempPipe);
-                }
             }
         }
 
+        //Dat kich thuoc cua camera bang row neu lon hon column hoac nguoc lai
         Camera.main.orthographicSize = Mathf.Max(_level.row, _level.column);
 
+        //Tinh toan va dat vi tri camera o giua level duoc tao ra
         Vector3 cameraPos = Camera.main.transform.position;
         cameraPos.x = _level.column /2f;
         cameraPos.y = _level.row /2f;
--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -7,7 +7,7 @@ using Random = UnityEngine.Random;
 public class Pipe : MonoBehaviour
 {
 
-    [HideInInspector] public bool IsFilled;
+    [HideInInspector] public bool isFilled;
     [HideInInspector] public int pipeType;
 
     [SerializeField] private Transform[] _pipePrefabs;
@@ -23,13 +23,24 @@ public class Pipe : MonoBehaviour
     private const int maxRotation = 3;
     private const int rotationMultiplier = 90;
 
+    /// <summary>
+    /// Ham khoi tao pipe
+    /// </summary>
+    /// <param name="pipe"></param>
     public void Init(int pipe)
     {
+        //Tao loai ong bang cach chia lay phan du
         pipeType = pipe % 10;
 
+        //Tao pipe la con cua Cell
         currentPipe = Instantiate(_pipePrefabs[pipeType], transform);
-        currentPipe.transform.localPosition = Vector3.zero;
+        currentPipe.transform.localPosition = Vector3.zero; // Dua pine ve vi tri mac dinh la (0,0) so voi Cell
 
+        //Neu la Cell rong thi khong lam gi
+        if (pipeType == 0)
+            return;
+
+        //Neu la pipe_1 hoac pipe_2 thi xoay theo ket qua chia lay phan nguyen neu la cac pipe con lai thi xoay random 90, 180, 270 do
         if (pipeType == 1 || pipeType == 2)
             rotation = pipe / 10;
         else
@@ -37,17 +48,18 @@ public class Pipe : MonoBehaviour
 
         currentPipe.transform.eulerAngles = new Vector3(0, 0, rotation * rotationMultiplier);
 
-        if (pipe == 0 || pipeType == 1)
-            IsFilled = true;
+        //Neu la pipe_1 thi se co nuoc
+        if (pipeType == 1)
+            isFilled = true;        
 
-        if (pipeType == 0)
-            return;
-
+        //Bat/tat sprite co nuoc cua pipe theo isFilled
         emptySprite = currentPipe.GetChild(0).GetComponent<SpriteRenderer>();
-        emptySprite.gameObject.SetActive(!IsFilled);
-        filledSprite = currentPipe.GetChild(1).GetComponent<SpriteRenderer>();
-        filledSprite.gameObject.SetActive(IsFilled);
+        emptySprite.gameObject.SetActive(!isFilled);
 
+        filledSprite = currentPipe.GetChild(1).GetComponent<SpriteRenderer>();
+        filledSprite.gameObject.SetActive(isFilled);
+
+        //Lay du lieu cac moi noi cua ong tu cac collider tao trong prefab
         connectBoxes = new List<Transform>();
         for (int i = 2; i < currentPipe.childCount; i++)
         {
